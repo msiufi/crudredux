5bb9dec8344ec6154a8fea1e9e7c4df5c5ca191d
Maximiliano Siufi <maxisiufi@gmail.com>
2021-06-22 13:09:21 -0300

    store

--- src/components/EditarProducto.js
+++ src/components/EditarProducto.js
@@ -2,7 +2,44 @@ import React from 'react';
 
 const EditarProducto = () => {
   return ( 
-    <h1>Hola soy EditarProducto</h1>
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Editar Producto
+            </h2>
+
+            <form action="">
+              <div className="form-group">
+                <label >Nombre Productos</label>
+                <input 
+                type="text" 
+                className="form-control" 
+                placeholder="Nombre Producto"
+                name="nombre"
+                />
+              </div>
+              <div className="form-group">
+                <label >Precio Productos</label>
+                <input 
+                type="text" 
+                className="form-control" 
+                placeholder="Precio Producto"
+                name="precio"
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >
+                Guardar Cambios
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
    );
 }
  
